c5d689fb3554c782bbcfbb3b70a67007755f7705
Viranga Jayawaradana <virangapasindu4@gmail.com>
2022-07-11 21:18:59 +0530

    labsheet 3 done

---
+++ .history/open-exe_20220711105449.py
@@ -0,0 +1,4 @@
+import cv2
+
+img_read = 'ColorCheck.png'
+img = cv2.imread()
---
+++ .history/open-exe_20220711105642.py
@@ -0,0 +1,6 @@
+import cv2
+import numpy as np
+img_read = 'ColorCheck.png'
+img = cv2.imread(img_read, cv2.IMREAD_COLOR)
+cv2.imshow('main',img)
+cv2.destroyAllWindow()
